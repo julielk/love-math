0412f2e7749cb8291f6dad5cbf5cbcda5418d1ff
juliet lokwiya <j_lokwiya@yahoo.com>
2023-12-30 17:06:15 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,8 @@ button.addEventListener("click", function() {
     alert("You clicked submit");
  }else{
         let gameType = this.getAttribute('data-type');
-        alert(`you clicked ${gameType}`);
+       // alert(`you clicked ${gameType}`);
+       runGame(gameType);
  }
                                                 
                                        
@@ -23,7 +24,7 @@ button.addEventListener("click", function() {
 }
 
 
-
+runGame('addition');
 
 })
 
@@ -33,7 +34,7 @@ and after the user  answers have been processed
 */
 
 
-function runGame(){
+function runGame(gameType){
 
    // create two random numbers between 1 nad 25
 
@@ -41,6 +42,16 @@ function runGame(){
 
    let num2 = Math.floor(Math.random() * 25) + 1;
 
+  if(gameType === 'addition'){
+
+   displayAdditionQuestion(num1,num2);
+
+  }
+    else{
+
+      alert(` unknown game type:  ${gameType}`);
+      throw `unknown game type: ${gameType}.Aborting`;
+    }
 
 
 }
@@ -69,9 +80,13 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1,operand2){
 
+document.getElementById('operand1').textContent = operand1;
 
+   document.getElementById('operand2').textContent = operand2;
+  
+   document.getElementById('operator').textContent = "+";
 
 }
 
