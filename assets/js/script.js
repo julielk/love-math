2066a3634bcd995d11eced8ddcdf4cdc7cadc744
juliet lokwiya <j_lokwiya@yahoo.com>
2023-12-30 10:41:01 +0000

    add event listener for document loading and  buttons clicked

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+
+let buttons = document.getElementsByTagName('button');
+
+for(let button of buttons){
+
+button.addEventListener("click", function() {
+
+ if(this.getAttribute('data-type')=== "submit"){
+
+
+    alert("You clicked submit");
+ }else{
+        let gameType = this.getAttribute('data-type');
+        alert(`you clicked ${gameType}`);
+ }
+                                                
+                                       
+
+})
+
+}
+
+
+
+
+})
+
+
+
+
+
+
+
+function runGame(){
+
+
+}
+
+function checkAnswer(){
+
+
+}
+
+
+function  calculateCorrectAnswer(){
+
+
+
+}
+
+function incrementScore(){
+
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function displayAdditionQuestion(){
+
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+
+}
+
+function displayMultiplyQuestion() {
+
+
+
+}
+
+
+/* create display divide function  */
