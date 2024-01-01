b76e6ea6a1004c474e7925dd93b16f9cb0631fdd
juliet lokwiya <j_lokwiya@yahoo.com>
2024-01-01 10:45:30 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,10 @@ function runGame(gameType) {
       displayAdditionQuestion(num1, num2);
 
    }
+   else if(gameType === 'multiply'){
+      displayMultiplyQuestion(num1,num2);
+   }
+
    else {
 
       alert(` unknown game type:  ${gameType}`);
@@ -110,6 +114,12 @@ function calculateCorrectAnswer() {
       return [operand1 + operand2, "addition"];
 
    }
+   else if(operator === "x"){
+
+        return [operand1 * operand2, "multiply"];
+
+
+   }
    else {
 
       alert(`Unimplemented operator  ${operator}`);
@@ -162,11 +172,18 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1,operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+
+   document.getElementById('operand2').textContent = operand2;
+
+   document.getElementById('operator').textContent = "x";
+
 
 
 
 }
 
 
-/* create display divide function  */
+/* create display divide function  challenge */
